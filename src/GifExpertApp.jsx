4e8f4f1cae5d0d0654df8e985b1aa6c3e8256c72
daviddiazh <daviddiazlds1221@gmail.com>
2022-07-09 23:24:07 -0500

    Making the form of filter data

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
+
+    const [ categories, setCategories ] = useState(['One Punch', 'Dragon Ball']);
+
+    const onAddCategory = () => {
+        setCategories([ ...categories, 'Valorant' ]);
+    }
+
     return (
         <>
-            <p>Hola mundo</p>
+            <h1>GifsApp</h1>
+
+            <AddCategory />
+            <button onClick={ onAddCategory }>Agregar</button>
+
+            <ol>
+                {
+                    categories.map( category => (
+                        <li key={category}>{category}</li>
+                    ))
+                }
+            </ol>
+
         </>
     )
 }
