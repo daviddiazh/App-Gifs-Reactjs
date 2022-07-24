55466a15a33fa3cf63297562493a658d79394e7a
daviddiazh <daviddiazlds1221@gmail.com>
2022-07-24 18:25:30 -0500

    Change of place the logic at new HOOK

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,21 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifItem } from './GifItem';
 
 export const GifGrid = ({ category }) => {
 
-    const [ images, setImages ] = useState([]);
-
-    const getImages = async () => {
-
-        const newImages = await getGifs(category);
-        setImages(newImages);
-
-    }
-
-    useEffect(() => {
-        getImages();
-    }, []);
+    const { images, isLoading } = useFetchGifs( category );
 
     return (
         <>
