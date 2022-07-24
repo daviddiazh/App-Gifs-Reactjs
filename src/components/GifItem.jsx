1c5533f8bc63f4d7a6f38daae9c33853446adc28
daviddiazh <daviddiazlds1221@gmail.com>
2022-07-24 18:43:37 -0500

    Adding loading

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export const GifItem = ({ title, url, id }) => {
+
     return (
         <div className='card'>
-            <img src={ url } alt={ title } />
+            <img src={ url } alt={ title }/>
             <p>{title}</p>
         </div>
     )
