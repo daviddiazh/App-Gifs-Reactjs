d01244ffc09c536eaeef9415b83400de0cc790bf
daviddiazh <daviddiazlds1221@gmail.com>
2022-08-07 19:31:55 -0500

    Shouldn't call onNewCategory if input value is empty <AddCategory /> Component

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -15,9 +15,10 @@ describe('Testing in <AddCategory /> Component', () => {
 
     test('Should call onNewCategory only if input contains a value', () => {
 
-        const inputValue = 'Coding testing'
+        const inputValue = 'Coding testing';
+        const onNewCategory = jest.fn();
 
-        render( <AddCategory onNewCategory={ () => {} } /> );
+        render( <AddCategory onNewCategory={ onNewCategory } /> );
 
         const input = screen.getByRole('textbox');
         const form = screen.getByRole('form');
@@ -26,6 +27,23 @@ describe('Testing in <AddCategory /> Component', () => {
         fireEvent.submit( form ); // Throw event submit (form)
         
         expect( input.value ).toBe(''); // Validate setInputValue = '' after final onSubmit
+
+        expect( onNewCategory ).toHaveBeenCalled(); // Verify that function is called.
+        expect( onNewCategory ).toHaveBeenCalledTimes(1); // Verify that function is called only once.
+        expect( onNewCategory ).toHaveBeenCalledWith( inputValue ); // Verify that function is called with input value.
+    });
+
+    test("Shouldn't call onNewCategory if input value is empty", () => {
+
+        const onNewCategory = jest.fn();
+        render( <AddCategory onNewCategory={ onNewCategory } /> );
+
+        const form = screen.getByRole('form');
+        fireEvent.submit( form );
+
+        expect( onNewCategory ).toHaveBeenCalledTimes(0);
+        expect( onNewCategory ).not.toHaveBeenCalled();
+        
     });
 
 });
